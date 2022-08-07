120f59be60c078b7ecdb9fb5c8c38f87930835af
Michael Kelly <mike@thegrumpydev.com>
2022-08-07 09:56:01 -0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  
+  boot.kernelModules = ["i2c-dev"];
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
