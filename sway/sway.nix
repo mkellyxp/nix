780bfaea7616db71a521bf245190d6940c8c2380
Mike Kelly <mike@membervault.co>
2023-05-18 09:36:56 -0700

    sway tools

--- sway/sway.nix
+++ sway/sway.nix
@@ -59,4 +59,4 @@
 # To get list of audio outputs and switch
 #   wpctl status
 #   wpctl set-default <ID>
-#   WIP: pw-dump | jq '.[0]' (to search outputs)
+#   WIP: pw-dump | jq '.[] | select(.info.props."media.class" == "Audio/Sink") | .id, .info.props."node.description", .info.state'
