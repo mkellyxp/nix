dfef88413090b5bbda3379f20801af5ecee015cd
Mike Kelly <mike@membervault.co>
2023-06-05 09:01:35 -0700

    stable kernel

--- common/system.nix
+++ common/system.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
     {
-    boot.kernelPackages = pkgs.linuxPackages_latest;
+    ## boot.kernelPackages = pkgs.linuxPackages_latest;
 
     services.fwupd.enable = true; 
 
@@ -16,12 +16,6 @@
         alsa.enable = true;
         alsa.support32Bit = true;
         pulse.enable = true;
-        # If you want to use JACK applications, uncomment this
-        #jack.enable = true;
-
-        # use the example session manager (no others are packaged yet so this is enabled by default,
-        # no need to redefine it in your config for now)
-        #media-session.enable = true;
     };
 
     ## Optional Things ##
