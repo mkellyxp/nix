01822bfdcada7555f5028ace4864f582077191b7
Mike Kelly <mike@membervault.co>
2023-08-03 08:58:07 -0700

    new beekeeper

--- common/beekeeper.nix
+++ common/beekeeper.nix
@@ -2,13 +2,13 @@
 
 let
   pname = "beekeeper-studio";
-  version = "3.9.9";
+  version = "3.9.20";
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "https://github.com/beekeeper-studio/ultimate-releases/releases/download/v${version}/Beekeeper-Studio-Ultimate-${version}.AppImage";
     name = "${pname}-${version}.AppImage";
-    sha512 = "3303e2bb0787d85f8789cd141291ea64b0bd9a0231cb8b87b58bd0efea3611d8c5662ff81a1e7079170c7e7318aebe23b657ca460a5e557600f45d3714e64f4d";
+    sha512 = "8491df84d59730442fffb9d294b6b2a3d1730f996f7a98021b2dc4550a24c62a38881913205d28a09a02a8b4c2e9f180cf00887b2a86ee12068f3a309de2baae";
   };
 
   appimageContents = appimageTools.extractType2 {
