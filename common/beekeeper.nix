27d5f3a215c2bbb343fda7def3c935776d509a04
Michael Kelly <mike@thegrumpydev.com>
2023-09-29 13:43:19 -0700

    new beekeeper

--- common/beekeeper.nix
+++ common/beekeeper.nix
@@ -2,13 +2,13 @@
 
 let
   pname = "beekeeper-studio";
-  version = "3.9.20";
+  version = "4.0.2";
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "https://github.com/beekeeper-studio/ultimate-releases/releases/download/v${version}/Beekeeper-Studio-Ultimate-${version}.AppImage";
     name = "${pname}-${version}.AppImage";
-    sha512 = "8491df84d59730442fffb9d294b6b2a3d1730f996f7a98021b2dc4550a24c62a38881913205d28a09a02a8b4c2e9f180cf00887b2a86ee12068f3a309de2baae";
+    sha512 = "005a91eadc6943fb05f6e4e342db0249dd5a302f02d717b064cb6052ab096cf87309a308d7a3c626123792a3c584dc8c7fb7dd214765993ff5ac15a22c5b6740";
     ## Run sha512sum Beekeeper-Studio-Ultimate-* to get checksum
   };
 
