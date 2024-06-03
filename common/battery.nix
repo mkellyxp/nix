816181e0789d15c225c574feb0327927dbd72395
Michael Kelly <mike@thegrumpydev.com>
2024-06-03 07:45:02 -0700

    battery stuff

--- common/battery.nix
+++ common/battery.nix
@@ -1,16 +1,49 @@
-{ config, pkgs, ... }:
+{ config, pkgs, ... }: {
 
-{
-    # services.power-profiles-daemon.enable = false;
-    services.auto-cpufreq.enable = true;
-    services.auto-cpufreq.settings = {
-      battery = {
-         governor = "powersave";
-         turbo = "never";
-      };
-      charger = {
-         governor = "performance";
-         turbo = "auto";
+    environment.systemPackages = with pkgs; [
+      powertop
+    ];
+  
+  powerManagement = {
+    enable = true;
+    powertop.enable = true;
+    cpuFreqGovernor = "powersave";
+  };
+  services = {
+    thermald.enable = true;
+    power-profiles-daemon.enable = false;
+    auto-cpufreq = {
+      enable = true;
+      settings = {
+        battery = {
+          governor = "powersave";
+          turbo = "never";
+        };
+        charger = {
+          governor = "powersave";
+          turbo = "auto";
+        };
       };
     };
+
+    udev.extraRules = ''
+      # Remove NVIDIA USB xHCI Host Controller devices, if present
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+      # Remove NVIDIA USB Type-C UCSI devices, if present
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+      # Remove NVIDIA Audio devices, if present
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+      # Remove NVIDIA VGA/3D controller devices
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+    '';
+  };
+
+  boot = {
+    extraModprobeConfig = ''
+      blacklist nouveau
+      options nouveau modeset=0
+    '';
+    blacklistedKernelModules =
+      [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
+  };
 }
