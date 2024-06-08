a300cc7327199ca7ad94a1a4f37c77bb96e42601
Michael Kelly <mike@membervault.co>
2024-06-08 15:08:59 -0700

    desktop icons and install script

---
+++ sway/tools/install.sh
@@ -0,0 +1,10 @@
+mkdir ~/.config
+mkdir ~/.local
+mkdir ~/.local/share
+
+ln -s ~/Projects/nix/sway/config/fuzzel ~/.config/fuzzel
+ln -s ~/Projects/nix/sway/config/gtk-3.0 ~/.config/gtk-3.0
+ln -s ~/Projects/nix/sway/config/gtk-4.0 ~/.config/gtk-4.0
+ln -s ~/Projects/nix/sway/config/sway ~/.config/sway
+
+ln -s ~/Projects/nix/helix ~/.config/helix
