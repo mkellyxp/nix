7485862f746a63c6bf11e6360cfe9f5b636e342b
Mike Kelly <mike@membervault.co>
2024-07-31 10:19:34 -0700

    Conditional battery status

--- sway/config/sway/status.sh
+++ sway/config/sway/status.sh
@@ -2,4 +2,8 @@ date_formatted=$(date "+%m/%d/%Y - %l:%M %p - ")
 
 battery_status=$(cat /sys/class/power_supply/BAT0/capacity)
 
-echo $battery_status% - $date_formatted 
+if [ -f "$battery_status_file" ]; then
+  echo $battery_status% - $date_formatted 
+else
+  echo $date_formatted 
+fi
