122cc56a7130b80868f8a3f15f4bac592c1516c3
Michael Kelly <mike@membervault.co>
2024-11-25 13:04:27 -0800

    latest kernel

--- common/system.nix
+++ common/system.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-    ## boot.kernelPackages = pkgs.linuxPackages_latest;
+    boot.kernelPackages = pkgs.linuxPackages_latest;
 
     boot.kernel.sysctl = { "vm.swappiness" = 10;};
 
