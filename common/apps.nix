f9ba7b2a4bd82da362aa6523bb25dad792b92ac1
Michael Kelly <mike@membervault.co>
2024-12-06 10:18:02 -0800

    cleanup

--- common/apps.nix
+++ common/apps.nix
@@ -1,30 +1,30 @@
 { config, lib, fetchurl, appimageTools, pkgs, ... }:
 
 {
-    environment.systemPackages = with pkgs; [
-		gcc
+  environment.systemPackages = with pkgs; [
+    gcc
     libnotify
-		firefox
-		neovim
-		helix
-		nil
+    firefox
+    neovim
+    helix
+    nil
     lazygit
     gitkraken
-		htop
-		fastfetch
+    htop
+    fastfetch
     vscode
     # zed-editor
-		git
-		appimage-run
-		vlc
-		stripe-cli
-		protonvpn-gui
-	  nerdfonts
-	  showmethekey
+    git
+    appimage-run
+    vlc
+    stripe-cli
+    protonvpn-gui
+    nerdfonts
+    showmethekey
     scrcpy
     obs-studio
-		(pkgs.callPackage ./beekeeper.nix { })
-    ];
+    (pkgs.callPackage ./beekeeper.nix { })
+  ];
     
   nixpkgs.overlays = [
     (final: prev: {
@@ -59,14 +59,3 @@
 # Split audio into smaller files 
 #	ffmpeg -i Zero\ G-1\ Chapter\ 1.mp3 -f segment -segment_time 600 -c copy chap1-%03d.mp3
 #
-# Sublime Text
-#	Control + Shift + p - Package Control
-#		- Install LSP
-#		- Install LSP Php
-#		- Install LSP tailwindcss
-#		- Install LSP elixir
-#   - Install HTML Syntax in Script
-#		- LSP Settings ( "lsp_format_on_save": true )
-#		- Key Bindings ( { "keys": ["ctrl+shift+s"], "command": "reveal_in_side_bar" } )
-#		- LSP Tailwind Settings ( "disabled_capabilities": { "colorProvider": true, }, )
-#		- Preference Settings ( "word_wrap": false, )
