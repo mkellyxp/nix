dfbea0fe118916c4b166bc708b304a10f547f3b8
Michael Kelly <mike@membervault.co>
2025-12-08 19:22:09 -0800

    Configures LEMP stack with virtual hosts
    Sets up Nginx virtual hosts for multiple local development sites.
    
    Includes configuration for PHP-FPM with increased file upload limits and
    Redis for caching, along with necessary socket permissions for Nginx and PHP.
    
    Adds nixbook.local to extraHosts.

--- common/lemp.nix
+++ common/lemp.nix
@@ -1,209 +1,240 @@
 { config, pkgs, ... }:
 
 {
-    environment.systemPackages = with pkgs; [
-    	php82
-		mysql80
-	    nodejs
-		nodePackages.intelephense
-		nodePackages.typescript-language-server
-		nodePackages.prettier
-		vscode-langservers-extracted
-		tailwindcss-language-server
-    ];
+  environment.systemPackages = with pkgs; [
+    php82
+    mysql80
+    nodejs
+    nodePackages.intelephense
+    nodePackages.typescript-language-server
+    nodePackages.prettier
+    vscode-langservers-extracted
+    tailwindcss-language-server
+  ];
 
-    networking.extraHosts = ''
-		127.0.0.1	course.local
-      	127.0.0.1   www.course.local
-		127.0.0.1   www.course2.local
-      	127.0.0.1   www.admin.local
-      	127.0.0.1   www.public.local
-      	127.0.0.1	www.dad.local
-      	127.0.0.1	www.design.local
-      	127.0.0.1	www.udon.local
-		127.0.0.1	www.upcycle.local
-    '';
+  networking.extraHosts = ''
+    127.0.0.1	course.local
+    127.0.0.1 www.course.local
+    127.0.0.1 www.course2.local
+    127.0.0.1 www.admin.local
+    127.0.0.1 www.public.local
+    127.0.0.1	www.dad.local
+    127.0.0.1	www.design.local
+    127.0.0.1	www.udon.local
+    127.0.0.1	www.upcycle.local
+    127.0.0.1	www.nixbook.local
+  '';
 
-    services.mysql = {
-  	    enable = true;
-  	    package = pkgs.mariadb;
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
+  };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."www.admin.local" = {
+      root = "/var/www/vip_member_vault_admin";
+      locations."= /" = {
+        extraConfig = ''
+                rewrite ^ /index.php;
+          	'';
+      };
+      locations."/".extraConfig = ''
+        			    try_files $uri $uri/ /index.php?$query_string;
+        		    '';
+      locations."~ \.php$".extraConfig = ''
+        			    fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
+        			    fastcgi_index index.php;
+        		    '';
     };
 
-    services.nginx = {
-  	    enable = true;
-	    virtualHosts."www.admin.local" = {
-		    root = "/var/www/vip_member_vault_admin";
-		    locations."= /" = {
-                extraConfig = ''
-                    rewrite ^ /index.php;
-              	'';
-            };
-		    locations."/".extraConfig = ''
-			    try_files $uri $uri/ /index.php?$query_string;
-		    '';
-		    locations."~ \.php$".extraConfig = ''
-			    fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
-			    fastcgi_index index.php;
-		    '';
-	    };
-
-		virtualHosts."course.local" = {
-		    root = "/var/www/vip_member_vault";
-		    locations."= /" = {
-                extraConfig = ''
-                    rewrite ^ /index.php;
-              	'';
-          	};
-		    locations."/".extraConfig = ''
-			    try_files $uri $uri/ /index.php?$query_string;
-		    '';
-		    locations."~ \.php$".extraConfig = ''
-			    fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
-			    fastcgi_index index.php;
-		    '';
-	    };
-
-	    virtualHosts."www.course.local" = {
-		    root = "/var/www/vip_member_vault";
-		    locations."= /" = {
-                extraConfig = ''
-                    rewrite ^ /index.php;
-              	'';
-          	};
-		    locations."/".extraConfig = ''
-			    try_files $uri $uri/ /index.php?$query_string;
-		    '';
-		    locations."~ \.php$".extraConfig = ''
-			    fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
-			    fastcgi_index index.php;
-		    '';
-	    };
-
-		virtualHosts."www.course2.local" = {
-		    root = "/var/www/vip_member_vault";
-		    locations."= /" = {
-                extraConfig = ''
-                    rewrite ^ /index.php;
-              	'';
-          	};
-		    locations."/".extraConfig = ''
-			    try_files $uri $uri/ /index.php?$query_string;
-		    '';
-		    locations."~ \.php$".extraConfig = ''
-			    fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
-			    fastcgi_index index.php;
-		    '';
-	    };
-
-	    virtualHosts."www.public.local" = {
-		    root = "/var/www/public_site_2023";
-		    locations."= /" = {
-                extraConfig = ''
-                	rewrite ^ /index.php;
-              	'';
-          	};
-		    locations."/".extraConfig = ''
-                rewrite ^/(.*)/$ /$1 permanent;
-			    try_files $uri $uri/ /$uri.php;
-		    '';
-		    locations."~ \.php$".extraConfig = ''
-			    fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
-			    fastcgi_index index.php;
-		    '';
-	    };
-
-	    virtualHosts."www.upcycle.local" = {
-		    root = "/var/www/upcyclecomputers";
-		    locations."= /" = {
-                extraConfig = ''
-                	rewrite ^ /index.php;
-              	'';
-          	};
-		    locations."/".extraConfig = ''
-                rewrite ^/(.*)/$ /$1 permanent;
-			    try_files $uri $uri/ /$uri.php;
-		    '';
-		    locations."~ \.php$".extraConfig = ''
-			    fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
-			    fastcgi_index index.php;
-		    '';
-	    };
-
-	    virtualHosts."www.design.local" = {
-		    root = "/var/www/membervault-html/public/admin";
-		    locations."= /" = {
-                extraConfig = ''
-                	rewrite ^ /index.php;
-              	'';
-          	};
-		    locations."~ \.php$".extraConfig = ''
-			    fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
-			    fastcgi_index index.php;
-		    '';
-	    };
-			
-	    virtualHosts."www.udon.local" = {
-		    root = "/var/www/udon";
-		    locations."= /" = {
-                extraConfig = ''
-                	rewrite ^ /index.php;
-              	'';
-          	};
-		    locations."~ \.php$".extraConfig = ''
-			    fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
-			    fastcgi_index index.php;
-		    '';
-	    };
+    virtualHosts."course.local" = {
+      root = "/var/www/vip_member_vault";
+      locations."= /" = {
+        extraConfig = ''
+                rewrite ^ /index.php;
+          	'';
+      };
+      locations."/".extraConfig = ''
+        			    try_files $uri $uri/ /index.php?$query_string;
+        		    '';
+      locations."~ \.php$".extraConfig = ''
+        			    fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
+        			    fastcgi_index index.php;
+        		    '';
     };
 
-	
-    services.phpfpm = {
-  	    pools.mypool = {
-		    	phpPackage = (pkgs.php82.withExtensions({ enabled, all }: enabled ++ [ all.tidy all.redis all.opcache ]));
-  		    user = "nobody";
-					phpOptions = ''
-			      upload_max_filesize = 512M
-			      post_max_size = 512M
-			      memory_limit = 512M
-			      '';	
-					
-			    settings = {
-				    pm = "dynamic";
-				    "listen.owner" = config.services.nginx.user;
-				    "pm.max_children" = 5;
-				    "pm.start_servers" = 2;
-				    "pm.min_spare_servers" = 1;
-				    "pm.max_spare_servers" = 3;
-				    "pm.max_requests" = 500;
-			    };
-  	    };
+    virtualHosts."www.course.local" = {
+      root = "/var/www/vip_member_vault";
+      locations."= /" = {
+        extraConfig = ''
+                rewrite ^ /index.php;
+          	'';
+      };
+      locations."/".extraConfig = ''
+        			    try_files $uri $uri/ /index.php?$query_string;
+        		    '';
+      locations."~ \.php$".extraConfig = ''
+        			    fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
+        			    fastcgi_index index.php;
+        		    '';
     };
-    
-		services.redis = {
-		  package = pkgs.redis;
-		  servers."" = {
-		    enable = true;
-		    port = 0;                                # no TCP
-		    unixSocket = "/run/redis/redis.sock";
-		    unixSocketPerm = 770;
 
-		    # bind = null;   # (implicit) not needed for socket-only
+    virtualHosts."www.course2.local" = {
+      root = "/var/www/vip_member_vault";
+      locations."= /" = {
+        extraConfig = ''
+                rewrite ^ /index.php;
+          	'';
+      };
+      locations."/".extraConfig = ''
+        			    try_files $uri $uri/ /index.php?$query_string;
+        		    '';
+      locations."~ \.php$".extraConfig = ''
+        			    fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
+        			    fastcgi_index index.php;
+        		    '';
+    };
 
-		    settings = {
-		      dir = "/var/lib/redis";
-		      save = [ "900 1" "300 10" "60 10000" ];
-		      appendonly = "yes";
-		      appendfsync = "everysec";
-		      maxmemory = "256mb";
-		      "maxmemory-policy" = "allkeys-lru";
-		    };
-		  };
-		};
+    virtualHosts."www.public.local" = {
+      root = "/var/www/public_site_2023";
+      locations."= /" = {
+        extraConfig = ''
+            	rewrite ^ /index.php;
+          	'';
+      };
+      locations."/".extraConfig = ''
+                        rewrite ^/(.*)/$ /$1 permanent;
+        			    try_files $uri $uri/ /$uri.php;
+        		    '';
+      locations."~ \.php$".extraConfig = ''
+        			    fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
+        			    fastcgi_index index.php;
+        		    '';
+    };
 
-		# allow PHP/Nginx to access the socket
-		users.users.nginx.extraGroups = [ "redis" ];
-		users.users.nobody.extraGroups = [ "redis" ];
-    
+    virtualHosts."www.upcycle.local" = {
+      root = "/var/www/upcyclecomputers";
+      locations."= /" = {
+        extraConfig = ''
+            	rewrite ^ /index.php;
+          	'';
+      };
+      locations."/".extraConfig = ''
+                        rewrite ^/(.*)/$ /$1 permanent;
+        			    try_files $uri $uri/ /$uri.php;
+        		    '';
+      locations."~ \.php$".extraConfig = ''
+        			    fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
+        			    fastcgi_index index.php;
+        		    '';
+    };
+
+    virtualHosts."www.nixbook.local" = {
+      root = "/var/www/nixbook-web";
+      locations."= /" = {
+        extraConfig = ''
+            	rewrite ^ /index.php;
+          	'';
+      };
+      locations."/".extraConfig = ''
+                  rewrite ^/(.*)/$ /$1 permanent;
+        			    try_files $uri $uri/ /$uri.php;
+        		    '';
+      locations."~ \.php$".extraConfig = ''
+        			    fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
+        			    fastcgi_index index.php;
+        		    '';
+    };
+
+    virtualHosts."www.design.local" = {
+      root = "/var/www/membervault-html/public/admin";
+      locations."= /" = {
+        extraConfig = ''
+            	rewrite ^ /index.php;
+          	'';
+      };
+      locations."~ \.php$".extraConfig = ''
+        			    fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
+        			    fastcgi_index index.php;
+        		    '';
+    };
+
+    virtualHosts."www.udon.local" = {
+      root = "/var/www/udon";
+      locations."= /" = {
+        extraConfig = ''
+            	rewrite ^ /index.php;
+          	'';
+      };
+      locations."~ \.php$".extraConfig = ''
+        			    fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
+        			    fastcgi_index index.php;
+        		    '';
+    };
+  };
+
+  services.phpfpm = {
+    pools.mypool = {
+      phpPackage = (
+        pkgs.php82.withExtensions (
+          { enabled, all }:
+          enabled
+          ++ [
+            all.tidy
+            all.redis
+            all.opcache
+          ]
+        )
+      );
+      user = "nobody";
+      phpOptions = ''
+        			      upload_max_filesize = 512M
+        			      post_max_size = 512M
+        			      memory_limit = 512M
+        			      '';
+
+      settings = {
+        pm = "dynamic";
+        "listen.owner" = config.services.nginx.user;
+        "pm.max_children" = 5;
+        "pm.start_servers" = 2;
+        "pm.min_spare_servers" = 1;
+        "pm.max_spare_servers" = 3;
+        "pm.max_requests" = 500;
+      };
+    };
+  };
+
+  services.redis = {
+    package = pkgs.redis;
+    servers."" = {
+      enable = true;
+      port = 0; # no TCP
+      unixSocket = "/run/redis/redis.sock";
+      unixSocketPerm = 770;
+
+      # bind = null;   # (implicit) not needed for socket-only
+
+      settings = {
+        dir = "/var/lib/redis";
+        save = [
+          "900 1"
+          "300 10"
+          "60 10000"
+        ];
+        appendonly = "yes";
+        appendfsync = "everysec";
+        maxmemory = "256mb";
+        "maxmemory-policy" = "allkeys-lru";
+      };
+    };
+  };
+
+  # allow PHP/Nginx to access the socket
+  users.users.nginx.extraGroups = [ "redis" ];
+  users.users.nobody.extraGroups = [ "redis" ];
+
 }
 
 ## NOTES ##
