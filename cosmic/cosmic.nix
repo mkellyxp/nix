bf3a5f6f0b1c04608f0b0649b615199d95311832
Michael Kelly <mike@membervault.co>
2025-12-10 15:54:50 -0800

    Standardizes list formatting in Nix configs
    Formats lists of kernel modules and system packages for improved readability and consistency across different Nix configurations. This change enhances maintainability by adhering to a consistent code style.

--- cosmic/cosmic.nix
+++ cosmic/cosmic.nix
@@ -1,29 +1,32 @@
 { config, pkgs, ... }:
 {
-    # Enable the X11 windowing system.
-    # services.xserver.enable = true;
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
 
-    # Enable the Cosmic Desktop Environment.
-    services.displayManager.cosmic-greeter.enable = true;
-    services.desktopManager.cosmic.enable = true;
-    services.desktopManager.cosmic.xwayland.enable = true;
+  # Enable the Cosmic Desktop Environment.
+  services.displayManager.cosmic-greeter.enable = true;
+  services.desktopManager.cosmic.enable = true;
+  # services.desktopManager.cosmic.xwayland.enable = true;
 
-    virtualisation.libvirtd.enable = true;
-    boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
+  virtualisation.libvirtd.enable = true;
+  boot.kernelModules = [
+    "kvm-amd"
+    "kvm-intel"
+  ];
 
-    environment.systemPackages = with pkgs; [
-        # gnomeExtensions.hide-universal-access
-        # gnomeExtensions.steal-my-focus-window
-        # gnomeExtensions.appindicator
-        # gnome-themes-extra
-        # gnome-tweaks
-        # gnome-boxes
-        # gnome-terminal
-        # gthumb
-        # papirus-icon-theme
-        # wmctrl
-        # wl-clipboard
-    ];
+  environment.systemPackages = with pkgs; [
+    # gnomeExtensions.hide-universal-access
+    # gnomeExtensions.steal-my-focus-window
+    # gnomeExtensions.appindicator
+    # gnome-themes-extra
+    # gnome-tweaks
+    # gnome-boxes
+    # gnome-terminal
+    # gthumb
+    # papirus-icon-theme
+    # wmctrl
+    # wl-clipboard
+  ];
 }
 
 ## NOTES
--- gnome/gnome.nix
+++ gnome/gnome.nix
@@ -1,46 +1,49 @@
 { config, pkgs, ... }:
 {
-    # Enable the X11 windowing system.
-    services.xserver.enable = true;
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
 
-    # Enable the GNOME Desktop Environment.
-    services.xserver.displayManager.gdm.enable = true;
-    services.xserver.desktopManager.gnome.enable = true;
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
-    virtualisation.libvirtd.enable = true;
-    boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
+  virtualisation.libvirtd.enable = true;
+  boot.kernelModules = [
+    "kvm-amd"
+    "kvm-intel"
+  ];
 
-    environment.systemPackages = with pkgs; [
-        gnomeExtensions.hide-universal-access
-        gnomeExtensions.steal-my-focus-window
-        gnomeExtensions.appindicator
-        gnome-themes-extra
-        gnome-tweaks
-        gnome-boxes
-        gnome-terminal
-        gthumb
-        papirus-icon-theme
-        wmctrl
-        wl-clipboard
-    ];
-    
-    nixpkgs.overlays = [
-        (final: prev: {
-          gitkraken = prev.gitkraken.overrideAttrs (oldAttrs: rec {
-            desktopItems = [
-              (prev.makeDesktopItem {
-                name = "GitKraken";
-                exec = "gitkraken --enable-features=UseOzonePlatform --ozone-platform=wayland";
-                icon = "gitkraken";
-                desktopName = "GitKraken Desktop";
-                genericName = "Git Client";
-                categories = [ "Development" ];
-                comment = "Unleash your repo";
-              })
-            ];
-          });
-        })
-    ];
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.hide-universal-access
+    gnomeExtensions.steal-my-focus-window
+    gnomeExtensions.appindicator
+    gnome-themes-extra
+    gnome-tweaks
+    gnome-boxes
+    gnome-terminal
+    gthumb
+    papirus-icon-theme
+    wmctrl
+    wl-clipboard
+  ];
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      gitkraken = prev.gitkraken.overrideAttrs (oldAttrs: rec {
+        desktopItems = [
+          (prev.makeDesktopItem {
+            name = "GitKraken";
+            exec = "gitkraken --enable-features=UseOzonePlatform --ozone-platform=wayland";
+            icon = "gitkraken";
+            desktopName = "GitKraken Desktop";
+            genericName = "Git Client";
+            categories = [ "Development" ];
+            comment = "Unleash your repo";
+          })
+        ];
+      });
+    })
+  ];
 }
 
 ## NOTES
